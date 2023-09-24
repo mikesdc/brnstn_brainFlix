41f70666390703fb4cb7019d60faa62372ac1687
Michael SDC <michael.sundelacruz@gmail.com>
2023-09-24 16:33:51 -0400

    more minor adjustments to styling

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -5,44 +5,38 @@ import { relativeTimestamp } from "../../App";
 import addCommentIcon from "../../assets/icons/add_comment.svg";
 import profilePhoto from "../../assets/images/Mohan-muruge.jpg";
 
-
-const Comments = ({selectedVideo}) => {
-  
+const Comments = ({ selectedVideo }) => {
   return (
     <>
-        <div className="comments__comments">
-          {selectedVideo.comments.length} Comments
-        </div>
+      <div className="comments__comments">
+        {selectedVideo.comments.length} Comments
+      </div>
 
-        <div className="comments__add-comment-container">
-          <div className="comments__add-comment-user-img">
-            <img src={profilePhoto} alt="user avater here" />
-          </div>
-          <div className="comments__add-comment-form-container">
-            <h1>JOIN THE CONVERSATION</h1>
-            <textarea placeholder="Add a new comment" />
-            <button>
-              <img src={addCommentIcon} alt="comment icon" />
-              COMMENT
-            </button>
-          </div>
+      <div className="comments__add-comment-container">
+        <div className="comments__add-comment-user-img">
+          <img src={profilePhoto} alt="user avater here" />
         </div>
+        <div className="comments__add-comment-form-container">
+          <h1>JOIN THE CONVERSATION</h1>
+          <textarea placeholder="Add a new comment" />
+          <button>
+            <img src={addCommentIcon} alt="comment icon" />
+            COMMENT
+          </button>
+        </div>
+      </div>
 
       {selectedVideo.comments.map((comment) => (
         <div className="comments-card" key={comment.id}>
           <div className="comments-card-user-img"></div>
           <div className="comments-card-container">
             <div className="comments-card-header">
-              <div className="comments-card-author">
-                {comment.name}
-              </div>
+              <div className="comments-card-author">{comment.name}</div>
               <div className="comments-card-date">
                 {relativeTimestamp(comment.timestamp)}
               </div>
             </div>
-            <div className="comments-card-comment">
-              {comment.comment}
-            </div>
+            <div className="comments-card-comment">{comment.comment}</div>
           </div>
         </div>
       ))}
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,23 +14,18 @@ const Header = (props) => {
         />
       </div>
 
-        <input
-          type="text"
-          className="header__search-bar"
-          placeholder="Search"
-        />
-        
+      <input type="text" className="header__search-bar" placeholder="Search" />
+
       <div className="header__upload-button buttons">
         <button className="buttons">
           <img src={uploadIcon} alt="" />
           UPLOAD
         </button>
       </div>
-      
-      
-      <div className="header__profile-icon"><img src={profilePhoto} alt="" className="header__profile-image" /></div>
-    
-    
+
+      <div className="header__profile-icon">
+        <img src={profilePhoto} alt="" className="header__profile-image" />
+      </div>
     </header>
   );
 };
