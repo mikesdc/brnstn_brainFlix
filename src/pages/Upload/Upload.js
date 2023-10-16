0ff04a239622287d5bfdecf42482a918385bca46
Michael SDC <michael.sundelacruz@gmail.com>
2023-10-15 23:26:11 -0400

    complete upload page

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -1,8 +1,9 @@
 import "./Upload.scss";
-import NextVideos from "../../components/NextVideos/NextVideos";
 import { useState, useEffect } from "react";
-import axios from 'axios';
-
+import axios from "axios";
+import uploadImg from "../../assets/images/Upload-video-preview.jpg";
+import uploadIcon from "../../assets/icons/upload.svg";
+import { Link } from "react-router-dom";
 
 function Upload() {
   const [selectedVideo, setSelectedVideo] = useState([]);
@@ -22,18 +23,64 @@ function Upload() {
       });
   }, []);
 
+  function handleSubmit() {
+    console.log("Upload");
+  }
+
   return (
-    <div className="App">
-      <main className="main">
-        <section className="main-section">
-          <div className="left-section">
+    <div className="upload">
+      <div className="upload__header">
+        <h1>Upload Video</h1>
+      </div>
+      <div className="upload__body">
+        <div className="upload__thumbnail">
+          <h2>VIDEO THUMBNAIL</h2>
+          <img src={uploadImg} alt="upload image thumbnail" />
+        </div>
+
+        <div className="upload__forms">
+          <div className="upload__forms-title">
+            <label htmlFor="title">TITLE YOUR VIDEO</label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              placeholder="Add a title to your video"
+              className="title"
+            />
           </div>
-          <NextVideos
-            videosList={modifiedVideoList}
-            setSelectedVideo={setSelectedVideo}
-          />
-        </section>
-      </main>
+          <div className="upload__forms-description">
+            <label htmlFor="desc">ADD A VIDEO DESCRIPTION</label>
+            <textarea
+              placeholder="Add a description to your video"
+              id="desc"
+              name="desc"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="upload__buttons">
+        <div className="upload__buttons-publish">
+          <Link
+            to="/"
+            onClick={() => {
+              handleSubmit();
+            }}
+            className="upload__button-link"
+          >
+            <button>
+              <img src={uploadIcon} alt="comment icon" />
+              PUBLISH
+            </button>{" "}
+          </Link>
+        </div>
+        <div className="upload__cancel-button">
+          <Link to="/" className="upload__cancel-link">
+            CANCEL
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
